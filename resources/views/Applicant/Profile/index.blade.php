882578b728d06b8e7bf0540d46382abca664ba96
C4P5LOCK <ji4seth@gmail.com>
2022-09-02 19:38:57 +0100

    Created Form Model, Worked on the Profile view for Applicants

--- resources/views/Applicant/Profile/index.blade.php
+++ resources/views/Applicant/Profile/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 
-<h1>Applicant Profile</h1>
+{{-- <h1>Applicant Profile</h1>
 
 @if($users)
 
@@ -13,13 +13,69 @@
 
     @endforeach
 
-    @endif
+    @endif --}}
+
+  <div class="row mt-5">
+    
+        <div class="col-md-3">
+
+            <div class="card card-primary card-outline">
+            <div class="card-body box-profile">
+            <div class="text-center">
+            <img class="profile-user-img img-fluid img-circle" src={{asset("/assets/dist/img/user2-160x160.jpg")}} alt="User profile picture">
+            </div>
+            <h3 class="profile-username text-center">{{auth()->user()->fname}}, {{auth()->user()->lname}}</h3>
+            
+            <ul class="list-group list-group-unbordered mb-3">
+            <li class="list-group-item">
+            <b>Status</b> <a class="float-right">Applied</a>
+            </li>
+            <li class="list-group-item">
+            <b>Verified</b> <a class="float-right">No</a>
+            </li>
+            <li class="list-group-item">
+            <b>Approved</b> <a class="float-right">No</a>
+            </li>
+            </ul>
+            <a href="#" class="btn btn-primary btn-block"><b>Follow</b></a>
+            </div>
+            
+            </div>
+            
+        </div>
+        
+        <div class="col-md-9">
+            <div class="card card-primary">
+            <div class="card-header">
+            <h3 class="card-title">About</h3>
+            </div>
+            
+            <div class="card-body">
+            <strong><i class="fas fa-book mr-1"></i> Education</strong>
+            <p class="text-muted">
+            B.S. in Computer Science from the University of Tennessee at Knoxville
+            </p>
+            <hr>
+            <strong><i class="fas fa-map-marker-alt mr-1"></i> Location</strong>
+            <p class="text-muted">Malibu, California</p>
+            <hr>
+            <strong><i class="fas fa-pencil-alt mr-1"></i> Skills</strong>
+            <p class="text-muted">
+            <span class="tag tag-danger">UI Design</span>
+            <span class="tag tag-success">Coding</span>
+            <span class="tag tag-info">Javascript</span>
+            <span class="tag tag-warning">PHP</span>
+            <span class="tag tag-primary">Node.js</span>
+            </p>
+            <hr>
+            <strong><i class="far fa-file-alt mr-1"></i> Notes</strong>
+            <p class="text-muted">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam fermentum enim neque.</p>
+            </div>
+            </div>
+        </div>
 
 
-
-
-
-
+  </div> <!--end of row-->
 
 
 @endsection
